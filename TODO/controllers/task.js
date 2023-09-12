18c8d973824c2354eade6b5c3747ef6b18500321
abhishek <abhishekthakur777191@gmail.com>
2023-09-12 13:48:55 +0530

    fixed-engin

--- TODO/controllers/task.js
+++ TODO/controllers/task.js
@@ -1,60 +1,76 @@
 const Task = require("../model/task");
 
 exports.newTask = async (req, res, next) => {
-  const { title, description } = req.body;
-  await Task.create({ title, description, user: req.user });
+  try {
+    const { title, description } = req.body;
+    await Task.create({ title, description, user: req.user });
 
-  res.status(200).json({
-    success: true,
+    res.status(200).json({
+      success: true,
 
-    message: "task created sucessfully",
-  });
+      message: "task created sucessfully",
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 exports.getMyTask = async (req, res) => {
-  const userid = req.user._id;
-  const tasks = await Task.find({ user: userid });
-  const { id } = req.params;
+  try {
+    const userid = req.user._id;
+    const tasks = await Task.find({ user: userid });
+    const { id } = req.params;
 
-  res.status(200).json({
-    success: true,
-    tasks,
-  });
+    res.status(200).json({
+      success: true,
+      tasks,
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 exports.updateTask = async (req, res) => {
-  const task = await Task.findById(req.params.id);
+  try {
+    const task = await Task.findById(req.params.id);
 
-  if (!task) {
-    return res.status(200).json({
+    if (!task) {
+      return res.status(200).json({
+        success: true,
+        message: "invalid id",
+      });
+    }
+
+    task.isCompleted = !task.isCompleted;
+    await task.save();
+
+    res.status(200).json({
       success: true,
-      message: "invalid id",
+      message: "task updated",
     });
+  } catch (error) {
+    console.log(error);
   }
-
-  task.isCompleted = !task.isCompleted;
-  await task.save();
-
-  res.status(200).json({
-    success: true,
-    message: "task updated",
-  });
 };
 
 exports.deleteTask = async (req, res) => {
-  const task = Task.findById(req.params.id);
+  try {
+    const task = Task.findById(req.params.id);
 
-  if (!task) {
-    return res.status(200).json({
+    if (!task) {
+      return res.status(200).json({
+        success: true,
+        message: "invalid id",
+      });
+    }
+
+    await task.deleteOne();
+
+    res.status(200).json({
       success: true,
-      message: "invalid id",
+      message: "task deleted",
     });
+  } catch (error) {
+    console.log(error);
   }
-
-  await task.deleteOne();
-
-  res.status(200).json({
-    success: true,
-    message: "task deleted",
-  });
 };
